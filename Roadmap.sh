10626142a4ebd1015256714694218c0cb5338e2b
matthcol <matthias.colin@gmail.com>
2024-04-02 15:46:14 +0200

    start project 03

--- Roadmap.sh
+++ Roadmap.sh
@@ -68,3 +68,45 @@ ansible -i hosts -u deploy -m ping all
 # after deployment: remove unlocked key from agent
 ssh-key -D
 
+# 1st day afternoon
+# user deploy as a variable
+# cleanup playbook(s)
+# - remove user from sudoers
+# - remove user from target hosts
+
+# https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_variables.html
+
+# with variable user_deploy inside playbook or in group_vars/all[.yml]
+ansible-playbook -i hosts -u deployer playbook-deployuser.yml 
+
+# group_vars/all[.yml] content
+user_deploy: deployer
+
+# alt.
+# group_vars/all.json content
+{
+    "user_deploy": "deploy"
+}
+
+ansible-playbook -i hosts -u deploy -e "user_deploy=deploy007" playbook-deployuser.yml 
+
+# Exercise: write a playbook to deactive deployment user sharing the same variable user_deploy
+ansible-playbook -i hosts -u deploy -e "user_deploy=deploy007" playbook-deactivateuser.yml
+# remove from sudoers user in group_vars/all.yml
+ansible-playbook -i hosts -u deploy playbook-deactivateuser.yml
+# fix the situation 
+ansible-playbook -i hosts -u srvadmin -k -K playbook-deployuser.yml
+
+# verbose mode
+ansible-playbook -i hosts -u deploy -v playbook-deployuser.yml 
+ansible-playbook -i hosts -u deploy -vv playbook-deployuser.yml 
+ansible-playbook -i hosts -u deploy -vvv playbook-deployuser.yml 
+# NB: we can see that ansible use a temporary directory on each target which is cleaned after
+ssh deploy@host1 "ls -al .ansible/tmp"
+
+# to resolve env variable on controller: ansible.builtin.env lookup
+# https://docs.ansible.com/ansible/latest/collections/ansible/builtin/env_lookup.html
+deployment_public_key: "{{ lookup('env', 'HOME') }}/.ssh/id_rsa_deploy.pub"
+deployment_public_key: "{{ lookup('ansible.builtin.env', 'HOME') }}/.ssh/id_rsa_deploy.pub"
+
+# Variables, magic variables, jinja filters
